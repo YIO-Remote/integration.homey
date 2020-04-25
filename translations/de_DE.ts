e12d43880dc69231078ee9a6b97de5c9561df31d
Workflow <hello@yio-remote.com>
2020-04-25 14:46:40 +0000

    Update file "/integration.homey/translated/*.ts" from "YIO-Remote/translations"

--- translations/de_DE.ts
+++ translations/de_DE.ts
@@ -6,12 +6,12 @@
     <message>
       <location filename="../src/homey.cpp" line="177"/>
       <source>Cannot connect to Homey.</source>
-      <translation type="unfinished">Cannot connect to Homey.</translation>
+      <translation>Verbindung zu Homey kann nicht hergestellt werden.</translation>
     </message>
     <message>
       <location filename="../src/homey.cpp" line="177"/>
       <source>Reconnect</source>
-      <translation type="unfinished">Reconnect</translation>
+      <translation>Erneut verbinden</translation>
     </message>
   </context>
 </TS>
