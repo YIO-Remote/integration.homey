1f433227a32c23d7bbb20ad088cfe57dd6ad236c
Workflow <hello@yio-remote.com>
2020-05-21 05:37:46 +0000

    ci: New translations

--- translations/lt_LT.ts
+++ translations/lt_LT.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../src/homey.cpp" line="160"/>
       <source>Failed to add entities from: %1</source>
-      <translation type="unfinished">Failed to add entities from: %1</translation>
+      <translation>Nepavyko pridėti objektų iš: %1</translation>
     </message>
     <message>
       <location filename="../src/homey.cpp" line="204"/>
