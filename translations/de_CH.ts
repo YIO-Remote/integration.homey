62fb6946aa4ad0cdfab0226173bf7de36907214c
Workflow <hello@yio-remote.com>
2020-05-21 11:47:45 +0000

    ci: New translations

---
+++ translations/de_CH.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de-CH" sourcelanguage="en">
+  <context>
+    <name>Homey</name>
+    <message>
+      <location filename="../src/homey.cpp" line="160"/>
+      <source>Failed to add entities from: %1</source>
+      <translation type="unfinished">Failed to add entities from: %1</translation>
+    </message>
+    <message>
+      <location filename="../src/homey.cpp" line="204"/>
+      <source>Cannot connect to Homey.</source>
+      <translation type="unfinished">Cannot connect to Homey.</translation>
+    </message>
+    <message>
+      <location filename="../src/homey.cpp" line="204"/>
+      <source>Reconnect</source>
+      <translation type="unfinished">Reconnect</translation>
+    </message>
+  </context>
+</TS>
