82a2dea264c2c3c72fd9d31f42005f076eb9a2f2
Workflow <hello@yio-remote.com>
2020-06-01 23:07:56 +0000

    ci: New translations

--- translations/zh_TW.ts
+++ translations/zh_TW.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../src/homey.cpp" line="160"/>
       <source>Failed to add entities from: %1</source>
-      <translation type="unfinished">Failed to add entities from: %1</translation>
+      <translation>從 %1 新增項目失敗</translation>
     </message>
     <message>
       <location filename="../src/homey.cpp" line="204"/>
